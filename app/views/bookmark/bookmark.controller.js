9c347bbea4e02f8496963653f44d020e10507c6f
Maximiliano Castro <max@axiomzen.co>
2016-09-04 23:51:54 -0300

    fix errors

--- app/views/bookmark/bookmark.controller.js
+++ app/views/bookmark/bookmark.controller.js
@@ -3,12 +3,12 @@
   angular.module('app.views.main')
     .controller('BookmarkController', BookmarkController);
 
-  function BookmarkController(bookmark) {
+  function BookmarkController(bookmark, $state) {
     var vm = this;
 
     vm.bookmark = bookmark;
     vm.cancelEditing = function () {
-      $state.go('root');
+      $state.go('main');
     }
   }
 })();
